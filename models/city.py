0774258c2df9309ec61f2dabca5fd73698bec8e1
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-24 18:38:59 +0100

    enhance code

--- models/city.py
+++ models/city.py
@@ -13,4 +13,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("place", backref="cities", cascade="delete")
+    places = relationship("Place", backref="cities", cascade="delete")
