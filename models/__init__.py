a4c3083673cdbe14c5ed362f9fb2518d6a31321d
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-24 18:38:59 +0100

    reslove type error

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
 if storage_t == "Db":
-    from models.engin.db_storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
