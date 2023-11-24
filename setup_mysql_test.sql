6cc4a13da90b6c0093814a402f3d0e510b160fef
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-24 18:38:59 +0100

    mysql test db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- databese hbnb_test_db
+-- username hbnb_test
+-- password hbnb_test_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
