1114c568eb57a0e7cb660f480d52e68235b8c00b
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-24 18:38:59 +0100

    update place func

--- models/place.py
+++ models/place.py
@@ -1,18 +1,69 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+if models.storage_t == 'Db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          ForignKey('places.id', onupdate='CASCADE', ondelete='CASCADE'),
+                          primary_key=True))
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if models.storage_t == 'Db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullalbe=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", seconday="place_amenity",
+                                 backref="place_amenities",
+                                 viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
+    
+    if models.storage_t != 'Db':
+        @property
+        def review(self):
+            """getter attribute returns the list of Review..."""
+            from models.review import Review
+            review_l =[]
+            review_a = models.storage.all(Review)
+            for review in review_a.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+        @property
+        def amenities(self):
+            """getter attribute returns the list of Amenity..."""
+            from models.amenity import Amenity
+            amenity_list = []
+            all_amenities = models.storage.all(Amenity)
+            for amenity in all_amenities.values():
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
