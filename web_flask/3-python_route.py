276876bb2475fb999eae07e7181244a45c5c6876
Ugwu Justine <ugwujustineobinna@gmail.com>
2024-01-03 08:11:40 +0100

    solve task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_hbnb(text):
+    text_result = text.replace('_', ' ')
+    return f'C {text_result}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_hbnb(text="is cool"):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
